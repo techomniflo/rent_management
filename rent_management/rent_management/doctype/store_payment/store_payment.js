ffba6c67644aa1c48c19c7d327fe3bc01a2cdfe5
Gourav Saini <gourav.saini@omniflo.in>
2022-09-08 12:28:08 +0530

    store payment doctype is created

---
+++ rent_management/rent_management/doctype/store_payment/store_payment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Gourav Saini and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Store Payment', {
+	// refresh: function(frm) {
+
+	// }
+});
