1b7cd480a9ca026eed0f9bd55ec2f07911781f45
Gourav Saini <gourav.saini@omniflo.in>
2022-09-12 12:46:43 +0530

    on_click get outstanding button get outstanding invoices

--- rent_management/rent_management/doctype/store_payment/store_payment.js
+++ rent_management/rent_management/doctype/store_payment/store_payment.js
@@ -5,4 +5,18 @@ frappe.ui.form.on('Store Payment', {
 	// refresh: function(frm) {
 
 	// }
+	get_outstanding(frm){
+		cur_frm.clear_table("invoices_reference");
+		cur_frm.refresh_field('invoices_reference');
+		frappe.call({
+			doc : frm.doc,
+			method : 'fetch_items',
+			freeze : true,
+			freeze_message : 'Getting All Items'
+		}).then((res) => {
+				refresh_field('invoices_reference');
+
+				
+		})
+	}
 });
--- rent_management/rent_management/doctype/store_payment/store_payment.py
+++ rent_management/rent_management/doctype/store_payment/store_payment.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2022, Gourav Saini and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class StorePayment(Document):
-	pass
+	@frappe.whitelist()
+	def fetch_items(self):
+		values={'company':self.company,'customer':self.customer}
+		sales_invoices=frappe.db.sql("""select si.name,si.posting_date,si.rounded_total,si.outstanding_amount,si.due_date from `tabSales Invoice` as si where si.company=%(company)s and si.customer=%(customer)s and si.outstanding_amount>0 and si.docstatus=1 and si.status!='Return' order by si.posting_date;""",values=values,as_dict=True)
+		for invoices in sales_invoices:
+			self.append('invoices_reference',{
+				'type':'Sales Invoice',
+				'invoice_name':invoices.name,
+				'grand_total':invoices.rounded_total,
+				'outstanding':invoices.outstanding_amount,
+				'due_date':invoices.due_date
+			})
