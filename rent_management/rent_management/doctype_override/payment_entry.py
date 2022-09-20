b6c7b5a36a211110102c2e18486e5789542780e3
Gourav Saini <gourav.saini@omniflo.in>
2022-09-20 15:16:12 +0530

    .

--- rent_management/rent_management/doctype_override/payment_entry.py
+++ rent_management/rent_management/doctype_override/payment_entry.py
@@ -3,7 +3,7 @@ import erpnext
 from frappe import ValidationError, _, qb, scrub, throw
 from frappe.utils import cint, comma_or, flt, getdate
 from erpnext.setup.utils import get_exchange_rate
-from erpnext import erpnext.accounts.doctype.payment_entry.payment_entry.get_reference_details as get_reference_details
+from erpnext.accounts.doctype.payment_entry.payment_entry import get_reference_details
 
 def override_get_reference_details(doc,method):
     @frappe.whitelist()
