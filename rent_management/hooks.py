33d3cf0c6678434103aa77a9b4df7ea82d76ac8f
Gourav Saini <gourav.saini@omniflo.in>
2022-09-20 14:40:15 +0530

    .

--- rent_management/hooks.py
+++ rent_management/hooks.py
@@ -105,13 +105,14 @@ doctype_js = {"Payment Entry": "public/payment_entry.js"}
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	"Payment Entry": {
+		"onload": "_rent_management.rent_management.doctype_override.payment_entry.override_get_reference_details",
+		"refresh": "_rent_management.rent_management.doctype_override.payment_entry.override_get_reference_details",
+		"reload": "_rent_management.rent_management.doctype_override.payment_entry.override_get_reference_details",
+        "before_insert":"_rent_management.rent_management.doctype_override.payment_entry.override_get_reference_details"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
@@ -142,10 +143,10 @@ doctype_js = {"Payment Entry": "public/payment_entry.js"}
 # Overriding Methods
 # ------------------------------
 #
-override_whitelisted_methods = {
-    "_erpnext.accounts.doctype.payment_entry.payment_entry.get_reference_details":"_rent_management.rent_management.doctype_override.payment_entry.get_reference_details"
+# override_whitelisted_methods = {
+#     "_erpnext.accounts.doctype.payment_entry.payment_entry.get_reference_details":"_rent_management.rent_management.doctype_override.payment_entry.get_reference_details"
 # 	"frappe.desk.doctype.event.event.get_events": "rent_management.event.get_events"
-}
+# }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
--- rent_management/rent_management/doctype_override/payment_entry.py
+++ rent_management/rent_management/doctype_override/payment_entry.py
@@ -1,9 +1,15 @@
 import frappe
 import erpnext
 from frappe import ValidationError, _, qb, scrub, throw
-from frappe.utils import cint, comma_or, flt, getdate, 
+from frappe.utils import cint, comma_or, flt, getdate
 from erpnext.setup.utils import get_exchange_rate
+from erpnext import erpnext.accounts.doctype.payment_entry.payment_entry.get_reference_details as get_reference_details
 
-@frappe.whitelist()
-def get_reference_details(reference_doctype, reference_name, party_account_currency):
-    pass
+def override_get_reference_details(doc,method):
+    @frappe.whitelist()
+    def override_get_reference_details(reference_doctype, reference_name, party_account_currency):
+        frappe.msgprint("hello")
+    get_reference_details = override_get_reference_details
+    
+    
+    
