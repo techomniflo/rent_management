c656ab70d5e4562a70e01e63da6bb6387b53ee18
Gourav Saini <gourav.saini@omniflo.in>
2022-09-20 12:07:03 +0530

    .

--- rent_management/rent_management/doctype/store_payment/store_payment.py
+++ rent_management/rent_management/doctype/store_payment/store_payment.py
@@ -75,7 +75,7 @@ class StorePayment(Document):
 	@frappe.whitelist()
 	def fetch_items(self):
 		values={'company':self.company,'customer':self.customer}
-		sales_invoices=frappe.db.sql("""select si.name,si.posting_date,si.rounded_total,si.outstanding_amount,si.due_date from `tabSales Invoice` as si where si.company=%(company)s and si.customer=%(customer)s and si.outstanding_amount!=0 and si.docstatus=1 and si.status!='Return' order by si.posting_date;""",values=values,as_dict=True)
+		sales_invoices=frappe.db.sql("""select si.name,si.posting_date,si.rounded_total,si.outstanding_amount,si.due_date from `tabSales Invoice` as si where si.company=%(company)s and si.customer=%(customer)s and si.outstanding_amount!=0 and si.docstatus=1 order by si.posting_date;""",values=values,as_dict=True)
 		for invoices in sales_invoices:
 			self.append('invoices_reference',{
 				'type':'Sales Invoice',
